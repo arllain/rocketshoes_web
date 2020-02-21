72cb41690fcefe837c54e15d658a0fcf137a4b89
arllainm <arllain@gmail.com>
2020-02-21 11:14:14 -0300

    Styling Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,93 @@
 import React from 'react';
-
-// import { Container } from './styles';
-
+import { ProductList } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 130,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 130,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 130,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 130,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 130,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 130,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
