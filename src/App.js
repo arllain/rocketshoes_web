0f02a9dc45a41af7ee5155c8fc03a1cbf4a5152d
arllainm <arllain@gmail.com>
2020-02-22 16:56:43 -0300

    Romoving product from cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './config/ReactotronConfig';
-import Routes from './routes';
 import GlobalStyles from './styles/global';
 import Header from './componets/Header';
-import { Provider } from 'react-redux';
+import Routes from './routes';
 import store from './store';
 
 function App() {
