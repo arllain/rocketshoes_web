fffa7902fb99f38db9da47f6f3be5b6cc0233c9b
arllainm <arllain@gmail.com>
2020-02-22 09:00:40 -0300

    Getting products from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,93 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ProductList } from './styles';
 import { MdAddShoppingCart } from 'react-icons/md';
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt="Shoes"
-        />
-        <strong>Shoes</strong>
-        <span>$130,00</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt="Shoes"
-        />
-        <strong>Shoes</strong>
-        <span>$130,00</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt="Shoes"
-        />
-        <strong>Shoes</strong>
-        <span>$130,00</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt="Shoes"
-        />
-        <strong>Shoes</strong>
-        <span>$130,00</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt="Shoes"
-        />
-        <strong>Shoes</strong>
-        <span>$130,00</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt="Shoes"
-        />
-        <strong>Shoes</strong>
-        <span>$130,00</span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button>
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>Add to cart</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
